82f61d1fd69aa89233a732875ec1081d95af2cef
AbdvlQadir <61961546+AbdvlQadir@users.noreply.github.com>
2020-03-26 22:37:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+<script>
+var myName = "Siyanbola Quadri";
+var myCourses = ['HTML','CSS','Javascript'];
+	document.write(myName);
+    document.write('<br>');
+	document.write(myCourses);
+    document.write('<br>');
+for (i=1;i<=200;i=i+2)
+{
+	document.write(i);
+    document.write('<br>');
+}
+
+</script>
